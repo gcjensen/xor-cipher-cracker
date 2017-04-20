de6ab4dc8e1d326538fa4e16a59f64768b3c9d50
George <gj4g13@soton.ac.uk>
2017-04-20 09:23:52 +0100

    Hex file is now read directly rather than requiring conversion to plaintext first

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ import sys
 import binascii
 from xor_cipher_cracker import XORCipherCracker
 
-def convert_hex_to_binary(hex):
-    hex = hex.replace(" ", "")
+def convert_hex_file_to_binary_string(hex_file):
+    hex_string = binascii.hexlify(hex_file).replace('\n', '')
     binary = [
          '0000','0001','0010','0011',
          '0100','0101','0110','0111',
@@ -11,17 +11,17 @@ def convert_hex_to_binary(hex):
          '1100','1101','1110','1111'
          ]
     binary_string = ''
-    for i in range(len(hex)):
-        binary_string += binary[int(hex[i], base=16)]
+    for i in range(len(hex_string)):
+        binary_string += binary[int(hex_string[i], base=16)]
     return binary_string
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as cipher_text_file:
-        cipher_text = cipher_text_file.read().replace('\n', '')
+    with open(sys.argv[1], 'r') as hex_file:
+        cipher_text_in_binary = convert_hex_file_to_binary_string(hex_file.read())
+        # the list of characters we believe the plaintext to contain
         plain_text_chars = 'abcdefghijklmnopqrstuvwxyz .,-\'?_()!'
-        cipher_text = convert_hex_to_binary(cipher_text)
         xor_cipher_cracker = XORCipherCracker(plain_text_chars)
-        possible_decryptions = xor_cipher_cracker.crack(cipher_text)
+        possible_decryptions = xor_cipher_cracker.crack(cipher_text_in_binary)
         for key in possible_decryptions.keys():
             print('Decryption with key: ' + hex(int(key, 2)))
             print(binascii.unhexlify('%x' % possible_decryptions[key]))
